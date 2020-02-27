c06a8e20f894cf011de2ec8f3a838bcef6a2e8dc
夏文宇 <11111@qq.com>
2020-02-27 23:32:57 +0800

    Initial commit

--- src/main/java/com/jiker/keju/AppRunner.java
+++ src/main/java/com/jiker/keju/AppRunner.java
@@ -4,11 +4,22 @@ import java.io.IOException;
 
 public class AppRunner {
 
-    public static void main(String[] args) throws NoSuchFieldException, IOException {
+    public static void main(String[] args) {
         String testDataFile = args[0];
+        String receipt = getstr(testDataFile);
+        System.out.println(receipt);
+    }
+
+    private static String getstr(String arg){
         TripalCalculate tripalCalculate = new TripalCalculate();
         String receipt = "";
-        receipt = tripalCalculate.calculateTaxiFree("src/main/resources/"+testDataFile);
-        System.out.println(receipt);
+        try {
+            receipt = tripalCalculate.calculateTaxiFree("src/main/resources/"+arg);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return receipt ;
     }
 }
