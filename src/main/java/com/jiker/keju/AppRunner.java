aa3a738c9155467b4ff383d0379d2b51147eaa24
夏文宇 <11111@qq.com>
2020-02-27 23:36:05 +0800

    Initial commit

--- src/main/java/com/jiker/keju/AppRunner.java
+++ src/main/java/com/jiker/keju/AppRunner.java
@@ -5,21 +5,14 @@ import java.io.IOException;
 public class AppRunner {
 
     public static void main(String[] args) {
-        String testDataFile = args[0];
-        String receipt = getstr(testDataFile);
-        System.out.println(receipt);
-    }
-
-    private static String getstr(String arg){
         TripalCalculate tripalCalculate = new TripalCalculate();
         String receipt = "";
         try {
-            receipt = tripalCalculate.calculateTaxiFree("src/main/resources/"+arg);
-        } catch (NoSuchFieldException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
+            receipt = tripalCalculate.calculateTaxiFree("src/main/resources/"+args[0]);
+        } catch (NoSuchFieldException | IOException e) {
             e.printStackTrace();
         }
-        return receipt ;
+        System.out.println(receipt);
     }
+
 }
