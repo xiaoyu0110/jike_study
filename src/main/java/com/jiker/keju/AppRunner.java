0dfe84232a55262b165b1e5689f48ae071f29077
夏文宇 <11111@qq.com>
2020-02-27 21:31:31 +0800

    Initial commit

--- src/main/java/com/jiker/keju/AppRunner.java
+++ src/main/java/com/jiker/keju/AppRunner.java
@@ -8,8 +8,7 @@ public class AppRunner {
           2. 你写的程序将把testDataFile作为参数加载此文件并读取文件内的测试数据，并对每条测试数据计算结果。
           3. 将所有计费结果拼接并使用\n分割，然后保存到receipt变量中。
          */
-//        String testDataFile = args[0];
-        String testDataFile = "testData.txt";
+        String testDataFile = args[0];
         TripalCalculate tripalCalculate = new TripalCalculate();
         String receipt = "";
         try {
